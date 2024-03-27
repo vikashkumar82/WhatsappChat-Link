e5e3bc08d367a98a0f70766fdb5ef93a4d555526
vikashkumar82 <avikashkumar82@gmail.com>
2024-03-27 13:27:24 +0530

    first commit

---
+++ emoji.js
@@ -0,0 +1,25 @@
+
+    $(document).ready(function() {
+	$("#emojionearea1").emojioneArea({
+  	
+		pickerPosition: "right",
+    tonesStyle: "bullet",
+		events: {
+         	keyup: function (editor, event) {
+           		console.log(editor.html());
+           		console.log(this.getText());
+        	}
+    	}
+	});
+  
+     $('#divOutside').click(function () {
+                $('.emojionearea-button').click()
+            })
+            
+            
+});
+
+$('#lala').click(function () {
+alert($("#emojionearea1").val());
+ $("#nnnnn").append($("#emojionearea1").val());
+})
